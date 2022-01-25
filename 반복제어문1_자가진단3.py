795d00d96ba006147c8d37d1cb72a08ada7598c5
Kim Do Hee <dohee.kimdh@gmail.com>
2022-01-25 13:39:11 +0900

    Add files via upload

--- 반복제어문1_자가진단3.py
+++ 반복제어문1_자가진단3.py
@@ -2,10 +2,10 @@
 # 출력하는 작업을 반복하다가 0이 입력되면 종료하는 프로그램을 작성하시오.
 
 while True:
-    num = int(input("number? "))
-    if num == 0:
+    num = int(input("number? ")) # 정수 입력받기
+    if num == 0: # 입력받은 정수가 0일 때 종료
         break;
-    elif num > 0:
+    elif num > 0: # 양수일 때
         print("positive integer")
-    else:
+    else: # 음수일 때
         print("negative number")
